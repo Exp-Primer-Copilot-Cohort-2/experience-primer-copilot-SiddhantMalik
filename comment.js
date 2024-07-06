c944eece042dc6e487727c731d922341bd5aabd9
Siddhant Malik <67875387+SiddhantMalik@users.noreply.github.com>
2024-07-06 09:17:05 +0000

    mod

--- comment.js
+++ comment.js
@@ -1,1 +1,18 @@
-// #hello AudioWorkletNode
+// Create web server
+
+// Import modules
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const app = express();
+const port = 3000;
+
+// Set up body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Set up static files
+app.use(express.static('public'));
+
+// Set up view engine
+app.set('view engine', 'ejs');
